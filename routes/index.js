9a9201a42ac766197c171f615a4e420ef7251483
Josua Öhler <jo026@hdm-stuttgart.de>
2017-12-08 14:21:23 +0200

    Modify the SQL String again.

--- routes/index.js
+++ routes/index.js
@@ -59,7 +59,7 @@ router.post('/insert', function (req, res, next) {
 
     con.connect(function(err) {
         if (err) throw err;
-        var sql_ = 'UPDATE Roomate SET peopleInside = ' + peopleInside + ', temperature = ' + temperature + ', lastUpdate = ' + timestamp + 'WHERE deviveID = ' + deviceId;
+        var sql_ = 'UPDATE Roomate SET peopleInside = ' + ${peopleInside} + ', temperature = ' + ${temperature} + ', lastUpdate = ' + ${timestamp} + 'WHERE deviveID = ' + ${deviceId};
         console.log(sql);
         con.query(sql, function (err, result) {
         if (err) throw err;
